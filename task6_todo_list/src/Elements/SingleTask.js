8384274c6f4e9df521b800e2c0d0566df39b7063
Elena <ivankina.es@gmail.com>
2021-08-10 13:03:31 +0300

    added styling

--- task6_todo_list/src/Elements/SingleTask.js
+++ task6_todo_list/src/Elements/SingleTask.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const SingleTask = ({ todoTask, todoID,todoComplete, toggleTask, deleteTask}) => {
     return (
- <div key={todoID}>
+ <div key={todoID} className="task-list">
+     <span className="spot">❍</span>
 <div className={todoComplete ? "item-text strike" : "item-text"} onClick={() => toggleTask(todoID)}>
      {todoTask}
 </div>
-<div onClick={() => deleteTask(todoID)}>X</div>
+
+<span className="close-buttton" onClick={() => deleteTask(todoID)}>X</span>
  </div>
     );
 };
